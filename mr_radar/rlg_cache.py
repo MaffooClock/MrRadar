MaffooClock/MrRadar

commit 1e253ccea347c1f000a0e7e43a12460e2a996c99

Make Site ID upper-case by default
We only need it lower-case in one place, so no reason to have sprinkled around `.upper()` everywhere else.

mr_radar/rlg_cache.py
@@ -34,7 +34,7 @@ class RLGCache:
         return path_exists and is_file
 
     def load( self, name ) -> None:
-        self._name = name
+        self._name = name.lower()
         self._json_file = f"{name}.json"
         self._pickledb = pickledb.load( self._json_file, False )
 

mr_radar/frame_generator.py
@@ -149,7 +149,7 @@ class FrameGenerator( RadarLoopGenerator ):
         text_y = self.axes.viewLim.y0 * 1.0025
         date_time = f"%s GMT" % str( grid.getDataTime().getRefTime() )
 
-        values = ( self.site_id.upper(), grid.getParameter(), grid.getLevel() or 'N/A' )
+        values = ( self.site_id, grid.getParameter(), grid.getLevel() or 'N/A' )
         frame_label = ( "%s (%s %s)" % values ).replace( ' N/A', '' )
 
         # Add the timestamp and product name at the bottom-center

mr_radar/radar_loop_generator.py
@@ -50,7 +50,7 @@ class RadarLoopGenerator:
     @site_id.setter
     def site_id( self, site_id: str ) -> None:
         self._validate_site_id( site_id )
-        self._site_id = site_id.lower()
+        self._site_id = site_id.upper()
         self.cache.load( self.site_id )
         logger.info( "→ Site ID is '{}'", self.site_id )
 
@@ -240,7 +240,7 @@ class RadarLoopGenerator:
 
         logger.info( "Retrieving coordinates for {}...", self.site_id )
 
-        request = DataAccessLayer.newDataRequest( 'obs', parameters=[ 'longitude', 'latitude' ], locationNames=[ self.site_id.upper() ] )
+        request = DataAccessLayer.newDataRequest( 'obs', parameters=[ 'longitude', 'latitude' ], locationNames=[ self.site_id ] )
         response = DataAccessLayer.getGeometryData( request, None )
 
         if not response:

commit 34d7939d2d8c9e46dc267a3997352311717631f2

Fixing regression in commit 1e253cc
Commit 1e253cc took care of keeping the site ID upper-case, but caused the JSON file to be named in upper-case, which was not the intent.

mr_radar/rlg_cache.py
@@ -15,7 +15,6 @@ class RLGCache:
     """
 
     def __init__( self ) -> None:
-        self._name = None
         self._pickledb = None
         self._dirty = False
         self._json_file = None
@@ -34,8 +33,7 @@ class RLGCache:
         return path_exists and is_file
 
     def load( self, name ) -> None:
-        self._name = name.lower()
-        self._json_file = f"{name}.json"
+        self._json_file = f"{name.lower()}.json"
         self._pickledb = pickledb.load( self._json_file, False )
 
     def get( self, key, default=None ) -> Any | None: